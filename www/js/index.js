776ca95fa006efba0150e3f288a6b4bfb315b12d
themaxtech <maxtechapps@gmail.com>
2015-09-12 13:51:19 +0530

    A36
    A36

--- www/js/index.js
+++ www/js/index.js
@@ -16,40 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-function initPushwoosh() {
-    var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
- 
-    //set push notification callback before we initialize the plugin
-    document.addEventListener('push-notification', function(event) {
-                                //get the notification payload
-                                var notification = event.notification;
- 
-                                //display alert to the user for example
-                                alert(notification.aps.alert);
-                               
-                                //clear the app badge
-                                pushNotification.setApplicationIconBadgeNumber(0);
-                            });
- 
-    //initialize the plugin
-    pushNotification.onDeviceReady({pw_appid:"PUSHWOOSH_APP_ID"});
-     
-    //register for pushes
-    pushNotification.registerDevice(
-        function(status) {
-            var deviceToken = status['deviceToken'];
-            console.warn('registerDevice: ' + deviceToken);
-            alert('Device Token -->: ' + deviceToken);
-        },
-        function(status) {
-            console.warn('failed to register : ' + JSON.stringify(status));
-            alert(JSON.stringify(['failed to register ', status]));
-        }
-    );
-     
-    //reset badges on app start
-    pushNotification.setApplicationIconBadgeNumber(0);
-}
+
+var pushNotification;
+pushNotification = window.plugins.pushNotification;
 
 var app = {
     SOME_CONSTANTS : false,  // some constant
@@ -67,11 +36,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.addEventListener('push-notification', function(event) {
-             var notification = event.notification;
-             alert(notification.aps.alert);
-             pushNotification.setApplicationIconBadgeNumber(0);
-        });
+        
     },
     initFastClick : function() {
         window.addEventListener('load', function() {
@@ -83,8 +48,8 @@ var app = {
 
     onDeviceReady: function() {
         console.log("device ready, start making you custom calls!"); 
-        alert(" Am ready"); 
-        initPushwoosh();
+        alert("Am ready"); 
+         
     } 
 
 };
