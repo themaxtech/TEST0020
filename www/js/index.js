bb2a83360cadd319a24d0e9ed514703910abfb33
themaxtech <maxtechapps@gmail.com>
2015-09-17 12:36:32 +0530

    AA
    AA

--- www/js/index.js
+++ www/js/index.js
@@ -1,73 +1,105 @@
-var BookIt = BookIt || {};
-
-// Begin boilerplate code generated with Cordova project.
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
     // Application Constructor
-    initialize: function () {
+    initialize: function() {
         this.bindEvents();
-        alert("ARUN: initialize");
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function () {
+    bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function () {
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
         app.receivedEvent('deviceready');
-         
     },
     // Update DOM on a Received Event
-    receivedEvent: function (id) {
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-    }
-};
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-app.initialize();
-
-// End boilerplate code.
-
-$(document).on("mobileinit", function (event, ui) {
-    $.mobile.defaultPageTransition = "slide";
-       alert("ARUN: deviceready");
-       try{
-       var pushNotification = window.plugins.pushNotification;
-    } catch(err) {
-        alert(err.message);
-         
-    }
-});
-
-app.signupController = new BookIt.SignUpController();
-
-//$(document).delegate("#page-signup", "pagebeforeshow", function () {
-//    // Reset the signup form.
-//    app.signupController.resetSignUpForm();
-//});
-
-$(document).on("pagecontainerbeforeshow", function (event, ui) {
-    if (typeof ui.toPage == "object") {
-        switch (ui.toPage.attr("id")) {
-            case "page-signup":
-                // Reset the signup form.
-                app.signupController.resetSignUpForm();
-                break;
+        console.log('Received Event: ' + id);
+        var pushNotification = window.plugins.pushNotification;
+        if (device.platform == 'android' || device.platform == 'Android') {
+            alert("Register called");
+            pushNotification.register(this.successHandler, this.errorHandler,{"senderID":"824841663931","ecb":"app.onNotificationGCM"});
+        }
+        else {
+            alert("Register called");
+            pushNotification.register(this.successHandler,this.errorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"app.onNotificationAPN"});
+        }
+    },
+    // result contains any message sent from the plugin call
+    successHandler: function(result) {
+        alert('Callback Success! Result = '+result)
+    },
+    errorHandler:function(error) {
+        alert(error);
+    },
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+    },
+    onNotificationAPN: function(event) {
+        var pushNotification = window.plugins.pushNotification;
+        alert("Running in JS - onNotificationAPN - Received a notification! " + event.alert);
+        
+        if (event.alert) {
+            navigator.notification.alert(event.alert);
+        }
+        if (event.badge) {
+            pushNotification.setApplicationIconBadgeNumber(this.successHandler, this.errorHandler, event.badge);
+        }
+        if (event.sound) {
+            var snd = new Media(event.sound);
+            snd.play();
         }
     }
-});
-
-$(document).delegate("#page-signup", "pagebeforecreate", function () {
-
-    app.signupController.init();
-
-    app.signupController.$btnSubmit.off("tap").on("tap", function () {
-        app.signupController.onSignupCommand();
-    });
-
-});
+};
